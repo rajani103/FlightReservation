2da1206dd3da705be7c272fd1ab5027e9e8b616d
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2022-08-28 15:36:58 +0530

    Update FlightReservationApp6ApplicationTests.java

--- src/test/java/com/psa/flightapp/FlightReservationApp6ApplicationTests.java
+++ src/test/java/com/psa/flightapp/FlightReservationApp6ApplicationTests.java
@@ -3,7 +3,7 @@ package com.psa.flightapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FlightReservationApp6ApplicationTests.class)
 class FlightReservationApp6ApplicationTests {
 
 	@Test
